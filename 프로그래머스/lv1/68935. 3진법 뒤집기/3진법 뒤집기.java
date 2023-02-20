9ca3fe4e3259bf45a03434619a0aa4e960648673
ParkGyeongWon <91941442+ParkGyeongWon@users.noreply.github.com>
2023-02-20 23:43:38 +0900

    [level 1] Title: 3진법 뒤집기, Time: 9.73 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        String Three = "";
+        
+        while(n>0){ //주어진 n을 인수분해 하여 문자열에 저장을한다.
+            
+            Three+=n%3;   
+            n/=3;
+            
+        }
+        
+        for(int i =Three.length()-1;i>=0;i--){ //  문자열의 끝에서 부터 3^0,3^1,3^2과 같이 문자열index 값과 곱하여 합을 구한다.
+            answer +=(Three.charAt(i)-'0')*Math.pow(3,Three.length()-1-i); //
+        }
+        
+    
+        
+        return answer;
+    }
+}
