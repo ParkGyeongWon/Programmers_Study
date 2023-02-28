61185257780fc10c72c5728230339a6db5081686
ParkGyeongWon <91941442+ParkGyeongWon@users.noreply.github.com>
2023-03-01 06:19:17 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,32 +1,30 @@
-import java.util.Arrays;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			int arr[] = new int[9];
-			
-			
-			Scanner sc = new Scanner(System.in);
-			
-			for(int i=0;i<arr.length;i++) {
-				arr[i] = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+        int[] arr = new int[9];
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for(int i=0;i<arr.length;i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int max = arr[0];
+		int maxOfPosition=0;
+		for(int i=1;i<arr.length;i++) {
+			if(max<arr[i]) {
+				max=arr[i];
+				maxOfPosition = i;
 			}
-			
-			int max = arr[0];
-			int maxOfPosition=0;
-			
-			for(int i=1;i<arr.length;i++) {
-				if(max<arr[i]) {
-					max=arr[i];
-					maxOfPosition=i;
-				}
-			}
-			
-			System.out.println(max);
-			System.out.println(maxOfPosition+1);
-			
+		}
+		
+		System.out.println(max);
+		System.out.println(maxOfPosition+1);
 			
 			
 	}
