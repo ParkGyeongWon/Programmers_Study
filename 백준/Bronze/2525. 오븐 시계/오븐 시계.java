1df5370ff80143b440554aff63949215f41454e0
ParkGyeongWon <91941442+ParkGyeongWon@users.noreply.github.com>
2023-03-01 06:07:20 +0900

    [Bronze III] Title: 오븐 시계, Time: 160 ms, Memory: 16028 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,18 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
 		int A,B,C; //시,분,초
 		int hour,minute; //시간 분
 		int min;
-		Scanner sc = new Scanner(System.in);
 		
-		A=sc.nextInt();
-		B=sc.nextInt();
-		C=sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st =new StringTokenizer(br.readLine()," ");
+		
+		
+		
+		A=Integer.parseInt(st.nextToken());
+		B=Integer.parseInt(st.nextToken());
+		C=Integer.parseInt(br.readLine());
+		
+		
 
 		min=A*60+B;
 		min +=C;
