13558921ebec902b9020a419f1d6a72e1a5f2b6f
ParkGyeongWon <91941442+ParkGyeongWon@users.noreply.github.com>
2023-03-02 20:42:59 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -1,15 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String s = sc.next();
+		
+		
+		String s = br.readLine();
 		
 		System.out.println(s.length());
+	
 	}
 
 }
