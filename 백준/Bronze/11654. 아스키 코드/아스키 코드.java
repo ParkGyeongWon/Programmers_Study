10b32932c5717a32ac8ab740be13ac0e89fa80f8
ParkGyeongWon <91941442+ParkGyeongWon@users.noreply.github.com>
2023-03-04 17:06:29 +0900

    [Bronze V] Title: 아스키 코드, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,18 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		int ascii;
 		
 		
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
-		ascii = sc.next().charAt(0);
+		ascii = br.readLine().charAt(0);
 		
 		
-		System.out.println(ascii);
+		sb.append(ascii);
+		
+		System.out.println(sb);
+		
 		
 		
 	}
