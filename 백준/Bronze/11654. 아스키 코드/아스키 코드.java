01295fac074db829f8cdff349484ddac76e221a6
ParkGyeongWon <91941442+ParkGyeongWon@users.noreply.github.com>
2023-03-04 17:02:13 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ascii;
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		ascii = sc.next().charAt(0);
+		
+		
+		System.out.println(ascii);
+		
+		
+	}
+
+}
